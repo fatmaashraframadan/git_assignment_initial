29592e568339a64b833d5be095a042787b1e1c2a
youssef mohamed mohamed shehata <y0y01311999@gmail.com>
2020-02-29 19:11:24 +0200

    20170349 changed circle volume function to get volume of a ball with a certain radius

--- src/CircleVol.java
+++ src/CircleVol.java
@@ -1,20 +1,14 @@
-
+import java.math.*;
 public class CircleVol implements ISubscriber {
     @Override
     public void notifySubscriber(String input) {
-        double cylinderVol =0;
-        double coneVol =0;
         double radius =Double.parseDouble(input);
         double pi= 3.142;
-        cylinderVol = pi * radius * radius * radius;
-        coneVol =(pi * radius * radius) *(radius / 3);
+        double fot = 4.0/3.0;
+        double circleVol =fot * (pi * Math.pow(radius,3)) ;
 
 
-        System.out.println("A circle has no volume since it is planar and has no height \n" );
-        System.out.println("if we consider the circle to be a part of a cylinder whose height equals"
-                + " the radius then \n  the volume of a cylinder whose circle radius is " + radius + " = " + cylinderVol +"\n" );
-        System.out.println("if we consider the circle to be a part of a cone whose height "
-                + "equals the radius then \n  the volume of a cone whose circle radius is " + radius + " = " + coneVol );
+        System.out.println("The volume of the ball whose radius is " + radius + " = " + circleVol + " \n" );
 
     }
 }
